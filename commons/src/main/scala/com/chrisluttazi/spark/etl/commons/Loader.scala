6f3433db3f1bbc23b49e5574b4acb6fe88a9538c
singh-anurag <33480243+singh-anurag@users.noreply.github.com>
2019-06-05 20:36:49 +0530

    Update Loader.scala
    the code now can assign start date and effective date to the records so it can be used to handle slowly changing dimensions data.

--- commons/src/main/scala/com/chrisluttazi/spark/etl/commons/Loader.scala
+++ commons/src/main/scala/com/chrisluttazi/spark/etl/commons/Loader.scala
@@ -1,44 +1,72 @@
 package com.chrisluttazi.spark.etl.commons
 
-import com.chrisluttazi.spark.etl.commons.enums.LoadType
-import com.chrisluttazi.spark.etl.commons.enums.LoadType.LoadType
-import org.apache.spark.sql.DataFrame
+import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.spark.{SparkConf, SparkContext}
 
-import scala.annotation.tailrec
-import scala.util.Try
+import org.apache.hadoop.fs._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
 
-trait Loader {
+import scala.io.Source
+import java.util.Calendar
+import java.text.SimpleDateFormat
 
-  /**
-    * Based on a list of ([[LoadType]] , [[String]], [[DataFrame]]) tries to write the dataframe
-    *
-    * @param list : list of ([[LoadType]] , [[String]], [[DataFrame]])
-    * @return
-    */
-  @tailrec
-  final def loadList(list: List[(LoadType, String, DataFrame)]): Try[Unit] = list match {
-    case nil => Try(1 == 1) //done
-    case head :: tail =>
-      if (load(head._1, head._2, head._3).isFailure)
-        1 == 1 //take action if failure
-      loadList(tail)
+// First run for Slowly changing Dimension
+// The code enables to check and load history table and handle slowly changing data by using start date and effective date
+object ScdFirstRun {
+  def main(args: Array[String]): Unit = {
 
+    println("SCD First run started")
+
+    val proc_dt = args(0)
+    
+    val inputFormat = new SimpleDateFormat("yyyyMMdd")
+    val reqFormat = new SimpleDateFormat("yyyy-MM-dd")
+
+    // Convert the effective and expiry date in yyyy-MM-dd format    
+
+    val eff_dt = reqFormat.format(inputFormat.parse(proc_dt))
+    val exp_dt = reqFormat.format(reqFormat.parse("2099-12-31"))    
+    
+    val histPath = new Path("/user/singhanurag/scala/hist/")
+    
+    val histTabPath = "/user/singhanurag/scala/hist/"
+
+    val input_file = s"/user/singhanurag/data/user_$proc_dt.csv"
+    
+    val conf = new SparkConf().setAppName("scd-2-first-run").setMaster("yarn-client")
+
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+    val fs = FileSystem.get(sc.hadoopConfiguration)
+
+    sqlContext.setConf("spark.sql.shuffle.partitions", "10")
+    sqlContext.setConf("spark.sql.orc.enabled","true")
+
+    import sqlContext.implicits._
+    
+    // Check if the HIST table directory present for first run, else create directory    
+
+    if (!fs.exists(histPath)) {
+      println("History table directory does not exist, creating directory")
+      fs.mkdirs(histPath)
+      
+    } else {
+            println("History table directory exist")
+            }
+
+    // Read User data from the file and set effective and expiry date
+    val user_data = sqlContext.read.format("com.databricks.spark.csv").
+                                    option("header", "true").
+                                    option("inferSchema", "true").
+                                    option("quote", "\"").
+                                    option("ignoreLeadingWhiteSpace", true).
+                                    load(input_file)
+
+    val user_data_hist = user_data.withColumn("eff_dt",to_date(lit(eff_dt),"yyyy-MM-dd")).withColumn("exp_dt",to_date(lit(exp_dt),"yyyy-MM-dd"))
+
+    // Save updated Yelp user data to HIST table 
+    user_data_hist.coalesce(2).write.mode(SaveMode.Overwrite).format("orc").save(histTabPath)
+
+    println("First run completed successfully")
   }
-
-  /**
-    * Load a single [[DataFrame]]
-    *
-    * @param loadType : SE see [[LoadType]]
-    * @param path     : SE
-    * @param df       : SE
-    * @return
-    */
-  def load(loadType: LoadType, path: String, df: DataFrame): Try[Unit] = Try(
-    loadType match {
-      case LoadType.ORC => df.write.orc(path)
-      case LoadType.PARQUET => df.write.parquet(path)
-      case LoadType.JSON => df.write.json(path)
-      case LoadType.CSV => df.write.csv(path)
-    }
-  )
-}
